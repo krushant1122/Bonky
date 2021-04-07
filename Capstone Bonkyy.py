8f7524a09c51c341776d2b5443495ce01508bd5f
Krushant <63834509+krushant1122@users.noreply.github.com>
2021-04-07 11:14:44 +0530

    Bonky V0.2
    Added search functionality and other browser commands

--- Capstone Bonkyy.py
+++ Capstone Bonkyy.py
@@ -6,6 +6,7 @@ import wikipedia
 import pyjokes
 from selenium import webdriver  
 from selenium.webdriver.chrome.options import Options
+import webbrowser
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -34,8 +35,7 @@ def take_command():
 
     except Exception as e:
             print(e)
-            print("Say that again sir")
-            return "None"  
+            print("Say that again sir")  
     return command
 
 
@@ -63,7 +63,7 @@ def run_Bonky():
     elif 'tell me your name' in command:
         talk('I am Bonky. Your deskstop Assistant')
     elif 'who made you' in command or 'who created you' in command:
-        talk('I have been first created by Krushant Patel,but multiple people helped me get better.')
+        talk('I have been created by Krushant Patel.')
     elif 'what is your name' in command or 'who are you' in command:
         talk('Did I forget to introduce myself? I am your personal assistant. Bonky!.')
     elif 'when is your birthday' in command:
@@ -72,13 +72,28 @@ def run_Bonky():
         talk('I’m stuck inside a device!! Help! Just kidding. I like it in here. Sometimes I hang out in the Cloud. It gives me a great view of the World Wide Web.')
     elif 'do you sleep' in command or 'when do you sleep' in command:
         talk('I take power naps when we arent talking.')
+    elif 'siri' in command or 'alexa' in command: 
+        talk('Im sorry but I dont do my friends work')   
     elif 'self-destruct' in command:
         talk('Commencing Self-Destruct protocol in T-minus 2 seconds Boom! Actually I think Ill stick around')
     elif 'what do you think about me' in command or 'what is your opinion about me' in command:
         talk('I think youre extremely cool')
     elif 'bye' in command:
 	    talk('Bye. Have a nice day!')
-	    exit()        
+	    exit()
+    elif 'open youtube' in command:
+        url = 'https://www.youtube.com/'
+        webbrowser.get('C:/Program Files/Google/Chrome/Application/chrome.exe %s').open(url) 
+    elif 'search' in command or 'google' in command or 'what is' in command or 'what are' in command:
+        search = command.replace('search', '').replace('what is', '').replace('what are', '')
+        talk(' searching ' + search)
+        pywhatkit.search(search) 
+    elif 'from wikipedia' in command:
+        talk('Checking the wikipedia ')
+        query = command.replace('wikipedia', '')
+        result = wikipedia.summary(query, sentences=4)
+        talk('According to wikipedia')
+        talk(result)                       
     else:
         talk('Please say the command again.')
         
